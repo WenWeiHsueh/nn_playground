9e315a87f51305e500ea505116d9613546233d26
royy1912 <royy1912@gmail.com>
2022-03-06 21:52:02 +0800

    Modified the layer structure and add a new conv layer

--- binarynet/mnist_cnn.py
+++ binarynet/mnist_cnn.py
@@ -12,14 +12,14 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, BatchNormalization, MaxPooling2D
 from keras.layers import Flatten
-from keras.optimizers import SGD, Adam, RMSprop
+from tensorflow.keras.optimizers import SGD, Adam, RMSprop
 from keras.callbacks import LearningRateScheduler
 from keras.utils import np_utils
-
+from keras import callbacks
 from binary_ops import binary_tanh as binary_tanh_op
 from binary_layers import BinaryDense, BinaryConv2D
-
-
+from binary_ops import binarize
+from keras import callbacks
 def binary_tanh(x):
     return binary_tanh_op(x)
 
@@ -39,7 +39,7 @@ pool_size = (2, 2)
 hidden_units = 128
 classes = 10
 use_bias = False
-
+#############################基本設定#############################
 # learning rate schedule
 lr_start = 1e-3
 lr_end = 1e-4
@@ -56,12 +56,12 @@ p2 = 0.5
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-X_train = X_train.reshape(60000, 1, 28, 28)
-X_test = X_test.reshape(10000, 1, 28, 28)
+X_train = X_train.reshape(60000, 1, 28, 28) ####調整資料大小
+X_test = X_test.reshape(10000, 1, 28, 28)   ##調整資料大小
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
+X_train /= 255    ##正規化
+X_test /= 255     ##正規化
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
@@ -69,53 +69,52 @@ print(X_test.shape[0], 'test samples')
 Y_train = np_utils.to_categorical(y_train, classes) * 2 - 1 # -1 or 1 for hinge loss
 Y_test = np_utils.to_categorical(y_test, classes) * 2 - 1
 
-
 model = Sequential()
 # conv1
-model.add(BinaryConv2D(128, kernel_size=kernel_size, input_shape=(channels, img_rows, img_cols),
+model.add(BinaryConv2D(32, kernel_size=kernel_size, input_shape=(channels, img_rows, img_cols),
                        data_format='channels_first',
                        H=H, kernel_lr_multiplier=kernel_lr_multiplier, 
                        padding='same', use_bias=use_bias, name='conv1'))
-model.add(BatchNormalization(epsilon=epsilon, momentum=momentum, axis=1, name='bn1'))
-model.add(Activation(binary_tanh, name='act1'))
-# conv2
-model.add(BinaryConv2D(128, kernel_size=kernel_size, H=H, kernel_lr_multiplier=kernel_lr_multiplier, 
-                       data_format='channels_first',
-                       padding='same', use_bias=use_bias, name='conv2'))
-model.add(MaxPooling2D(pool_size=pool_size, name='pool2', data_format='channels_first'))
+model.add(BinaryConv2D(32, kernel_size=kernel_size, H=H, kernel_lr_multiplier=kernel_lr_multiplier,                         data_format='channels_first',
+                        padding='same', use_bias=use_bias, name='conv2'))
+model.add(BinaryConv2D(32, kernel_size=kernel_size, H=H, kernel_lr_multiplier=kernel_lr_multiplier,
+                        data_format='channels_first',
+                        padding='same', use_bias=use_bias, name='conv3'))
+model.add(MaxPooling2D(pool_size=pool_size, name='pool2', data_format='channels_last'))
 model.add(BatchNormalization(epsilon=epsilon, momentum=momentum, axis=1, name='bn2'))
 model.add(Activation(binary_tanh, name='act2'))
-# conv3
-model.add(BinaryConv2D(256, kernel_size=kernel_size, H=H, kernel_lr_multiplier=kernel_lr_multiplier,
-                       data_format='channels_first',
-                       padding='same', use_bias=use_bias, name='conv3'))
-model.add(BatchNormalization(epsilon=epsilon, momentum=momentum, axis=1, name='bn3'))
-model.add(Activation(binary_tanh, name='act3'))
-# conv4
-model.add(BinaryConv2D(256, kernel_size=kernel_size, H=H, kernel_lr_multiplier=kernel_lr_multiplier,
-                       data_format='channels_first',
-                       padding='same', use_bias=use_bias, name='conv4'))
-model.add(MaxPooling2D(pool_size=pool_size, name='pool4', data_format='channels_first'))
-model.add(BatchNormalization(epsilon=epsilon, momentum=momentum, axis=1, name='bn4'))
-model.add(Activation(binary_tanh, name='act4'))
+#model.add(BatchNormalization(epsilon=epsilon, momentum=momentum, axis=1, name='bn1'))
+model.add(Activation(binary_tanh, name='act1'))
 model.add(Flatten())
-# dense1
-model.add(BinaryDense(1024, H=H, kernel_lr_multiplier=kernel_lr_multiplier, use_bias=use_bias, name='dense5'))
+model.add(Dense(32, activation='relu'))
 model.add(BatchNormalization(epsilon=epsilon, momentum=momentum, name='bn5'))
-model.add(Activation(binary_tanh, name='act5'))
-# dense2
-model.add(BinaryDense(classes, H=H, kernel_lr_multiplier=kernel_lr_multiplier, use_bias=use_bias, name='dense6'))
-model.add(BatchNormalization(epsilon=epsilon, momentum=momentum, name='bn6'))
-
+model.add(Dense(10, activation='softmax'))
 opt = Adam(lr=lr_start) 
-model.compile(loss='squared_hinge', optimizer=opt, metrics=['acc'])
+model.compile(loss='squared_hinge', optimizer=opt, metrics=['accuracy'])
 model.summary()
-
 lr_scheduler = LearningRateScheduler(lambda e: lr_start * lr_decay ** e)
 history = model.fit(X_train, Y_train,
-                    batch_size=batch_size, epochs=epochs,
-                    verbose=1, validation_data=(X_test, Y_test),
-                    callbacks=[lr_scheduler])
+                    batch_size=50, epochs=1,
+                    verbose=1, validation_data=(X_test, Y_test))
 score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
+print(model.layers[0].get_weights())
+binary_kernel = binarize(model.layers[0].kernel, H=model.layers[0].H) 
+binary_kernel_1 = binarize(model.layers[1].kernel, H=model.layers[1].H)
+print('BINARIZE_1')
+print(binary_kernel)
+print('BINARIZE_2')
+print(binary_kernel_1)
+
+array = binary_kernel.numpy()
+weight_file = open('weight_1.txt', 'a')
+for i in range(len(binary_kernel)):
+  weight_file.write(str(array[i]) + '\n')
+weight_file.close()
+
+array_1 = binary_kernel_1.numpy()
+weight_file_1 = open('weight_2.txt', 'a')
+for i in range(len(binary_kernel_1)):
+  weight_file_1.write(str(array_1[i]) + '\n')
+weight_file_1.close()
